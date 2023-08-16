17d38c8f9b2eb3090d0a22d3f17a005087a9b892
lanit-com-vn <136077802+lanit-com-vn@users.noreply.github.com>
2023-08-16 10:02:32 +0700

    ipv6-with-port-none-password-64.sh

--- ipv6-with-port-none-password-64.sh
+++ ipv6-with-port-none-password-64.sh
@@ -96,7 +96,7 @@ while :; do
     echo "Number out of range, try again"
   fi
 done
-LAST_PORT=$(($FIRST_PORT + 512))
+LAST_PORT=$(($FIRST_PORT + 1000))
 echo "LAST_PORT is $LAST_PORT. Continue..."
 
 gen_data >$WORKDIR/data.txt
