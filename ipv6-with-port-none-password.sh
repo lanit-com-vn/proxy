2f390206c3ff362da1a1b91a990ffb84842ada23
lanit-com-vn <136077802+lanit-com-vn@users.noreply.github.com>
2023-08-17 15:11:39 +0700

    ipv6-with-port-none-password.sh

--- ipv6-with-port-none-password.sh
+++ ipv6-with-port-none-password.sh
@@ -108,7 +108,6 @@ chmod +x $WORKDIR/boot_*.sh
 cp $WORKDIR/boot_*.sh /etc/profile.d/
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
-cat >>/etc/profile.d/ <<EOF
 bash ${WORKDIR}/boot_iptables.sh
 bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 10048
